527e5bce55e69852c6d6b97ed7a3edede7ca60c2
EricDevelopment <35014703+EricDevelopment@users.noreply.github.com>
2018-03-17 10:46:19 -0400

    Fixed it so this actually works right.

--- makeLabelFiles.sh
+++ makeLabelFiles.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #Eric Smith
 #This script takes a directory of training images as
-#a command-line argument
+#a command-line argument (must be .jpg)
 #It then makes a sub-directory called "LabelData"
 #With one blank text file for each .jpg in the original directory
 #for use with the euclid.py labelling tool
 
-#check that filpath is supplied
+#check that filepath is supplied
 if [ $# -eq 0 ]
   then
     echo "No filepath to image directory supplied"
@@ -18,5 +18,6 @@ mkdir -p $1/LabelData
 for f in $1*.jpg
 do
   filename=$(basename "$f")
+  filename="${filename%.*}"
   touch "$1LabelData/$filename.txt"
 done
